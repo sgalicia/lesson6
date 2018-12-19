6073d45108adc8a2b1c4799d5fbf7db3ca9c612f
sgalicia <44712840+sgalicia@users.noreply.github.com>
2018-12-19 14:50:16 -0500

    Add files via upload

---
+++ Lesson 6/problem2.py
@@ -0,0 +1,11 @@
+name = input("What is your name? ")
+print("Nice to meet you " + name + ", my name is Chatbot.")
+
+age = input("How old are you? ") 
+print("Wow, " + age + " years old! I was born today.")
+
+feeling = input("How are you feeling today? ")
+print("Glad to hear you are feeling " + feeling + ". That is wonderful to hear!")
+
+trick = input("Trick or Treat? ")
+print('Cool.')
